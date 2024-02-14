bf2d4aea36564f6b83bc80efdf5e8f3642ada715
mario <codemario318@gmail.com>
2024-02-14 23:44:40 +0900

    [UPDATE] python > leetcode > 1004

--- python3/leetcode/1004_max_consecutive_ones_3.py
+++ python3/leetcode/1004_max_consecutive_ones_3.py
@@ -27,21 +27,15 @@ nums[i] is either 0 or 1.
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
         start = 0
-        zeroCount = 0
-        length = maxLength = 0
 
-        for i in range(len(nums)):
-            length += 1
-            zeroCount += 1 if nums[i] == 0 else 0
+        for end in range(len(nums)):
+            if nums[end] == 0:
+                k -= 1
 
-            while zeroCount > k and start <= i:
+            if k < 0:
                 if nums[start] == 0:
-                    zeroCount -= 1
-
+                    k += 1
                 start += 1
-                length -= 1
 
-            maxLength = max(maxLength, length)
-
-        return maxLength
+        return end - start + 1
     
