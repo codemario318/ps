8ce6483c0894aebddccd7f823260a23fa457a282
mario <codemario318@gmail.com>
2024-04-09 21:38:43 +0900

    [UPDATE] java > programmers > 택배상자

--- java/src/programmers/택배상자/Solution.java
+++ java/src/programmers/택배상자/Solution.java
@@ -1,52 +1,23 @@
 package programmers.택배상자;
-
 import java.util.*;
 
 class Solution {
     public int solution(int[] order) {
-        int answer = 0;
-
-        Queue<Integer> queue = new LinkedList<>();
         Stack<Integer> stack = new Stack<>();
-
-        for (int n = 1; n <= order.length; n++) {
-            queue.add(n);
-        }
-
         int i = 0;
 
-        while (!queue.isEmpty() && i < order.length) {
-            int target = order[i];
+        for (int n = 1; n <= order.length; n++) {
+            stack.push(n);
 
-            if (target == queue.peek()) {
-                answer++;
-                i++;
-                queue.poll();
-                continue;
+            while(!stack.isEmpty()) {
+                if (stack.peek() == order[i]) {
+                    stack.pop();
+                    i++;
+                } else
+                    break;
             }
-
-            if (!stack.isEmpty() && stack.peek() == target) {
-                stack.pop();
-                answer++;
-                i++;
-                continue;
-            }
-
-            stack.push(queue.poll());
         }
 
-        while (!stack.isEmpty() && i < order.length) {
-            int target = order[i];
-
-            if (stack.peek() != target) {
-                break;
-            }
-
-            stack.pop();
-            answer++;
-            i++;
-        }
-
-        return answer;
+        return i;
     }
 }
