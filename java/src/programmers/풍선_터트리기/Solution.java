ecf8464adef3c61a715525a9ccaaac0a19778051
mario <codemario318@gmail.com>
2024-04-11 15:44:08 +0900

    [SOLVED] java > programmers > 풍선 터트리기

---
+++ java/src/programmers/풍선_터트리기/Solution.java
@@ -0,0 +1,31 @@
+package programmers.풍선_터트리기;
+
+class Solution {
+    public int solution(int[] a) {
+        int length = a.length;
+        int[] memLeft = new int[length];
+        int[] memRight = new int[length];
+
+        memLeft[0] = a[0];
+
+        for(int i = 1; i < length; i++) {
+            memLeft[i] = Math.min(a[i], memLeft[i - 1]);
+        }
+
+        memRight[length - 1] = a[length - 1];
+
+        for(int i = length - 2; i >= 0; i--) {
+            memRight[i] = Math.min(a[i], memRight[i + 1]);
+        }
+
+        int count = length;
+
+        for(int i = 0; i < length; i++) {
+            if (a[i] > memLeft[i] && a[i] > memRight[i]) {
+                count--;
+            }
+        }
+
+        return count;
+    }
+}
