7e64bd2de071b8b89f6e22cccf77653b37f45376
mario <codemario318@gmail.com>
2024-04-11 21:42:46 +0900

    [UPDATE] java > programmers > 다단계 칫솔 판매 > 코드 개선

--- java/src/programmers/다단계_칫솔_판매/Solution.java
+++ java/src/programmers/다단계_칫솔_판매/Solution.java
@@ -2,54 +2,57 @@ package programmers.다단계_칫솔_판매;
 
 import java.util.*;
 
+class Member {
+    static final double SHARE_RATE = 0.1;
+
+    private Member parant;
+    private int money;
+
+    Member(Member parant, int money) {
+        this.parant = parant;
+        this.money = money;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void addMoney(int money) {
+        int share = (int) (money * SHARE_RATE);
+
+        this.money += money - share;
+
+        if (share > 0 && parant != null) {
+            parant.addMoney(share);
+        }
+    }
+}
+
 class Solution {
-    private static int PRICE = 100;
-    private Map<String, Integer> moneys = new HashMap<>();
-    private Map<String, String> parantTree = new HashMap<String, String>();
+    private static final int PRICE = 100;
+    private Map<String, Member> members = new HashMap<>();
 
     public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
-        moneys.put("-", 0);
+        members.put("-", new Member(null, 0));
 
         for(int i = 0; i < enroll.length; i++) {
-            String e = enroll[i];
-            String r = referral[i];
+            String childName = enroll[i];
+            String parantName = referral[i];
 
-            moneys.put(e, 0);
-            parantTree.put(e, r);
+            Member child = new Member(members.get(parantName), 0);
+
+            members.put(childName, child);
         }
 
         for(int i = 0; i < seller.length; i++) {
-            String child = seller[i];
+            String name = seller[i];
             int count = amount[i];
 
-            setMoney(child, count * PRICE);
+            members.get(name).addMoney(count * PRICE);
         }
 
-        int[] answer = new int[enroll.length];
-
-        for(int i = 0; i < enroll.length; i++) {
-            String name = enroll[i];
-            answer[i] = moneys.get(name);
-        }
-
-        return answer;
-    }
-
-    private void setMoney(String child, int money) {
-        int childMoney = moneys.get(child);
-        int shareMoney = (int) (money * 0.1);
-
-        if (shareMoney < 1) {
-            moneys.put(child, childMoney + money);           
-            return;
-        }
-
-
-        moneys.put(child, childMoney + money - shareMoney);
-
-        if (parantTree.containsKey(child)) {
-            String parant = parantTree.get(child);
-            setMoney(parant, shareMoney);
-        }       
+        return Arrays.stream(enroll)
+            .mapToInt(name -> members.get(name).getMoney())
+            .toArray();
     }
 }
