f8c897916a9e716601646e407f1f705b459a229b
mario <codemario318@gmail.com>
2024-04-27 00:01:06 +0900

    [UPDATE] python > boj > 11727. 2 x n 타일링 2

--- python3/boj/11727_2xn_타일링_2.py
+++ python3/boj/11727_2xn_타일링_2.py
@@ -1,11 +1,17 @@
 SIZE = 1001
 MOD = 10007
 
+MEM = [-1 for _ in range(SIZE + 1)]
+MEM[0], MEM[1], MEM[2] = 0, 1, 3
+
+def dp(n):
+    if MEM[n] >= 0:
+        return MEM[n]
+
+    MEM[n] = (dp(n - 1) + 2 * dp(n - 2)) % MOD
+
+    return MEM[n]
+
 if __name__ == "__main__":
     n = int(input())
-    mem = [0, 1, 3]
-    
-    for _ in range(3, n + 1):
-        mem.append((mem[-1] + 2 * mem[-2]) % MOD) 
-    
-    print(mem[n])
+    print(dp(n))
